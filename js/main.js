8d07fea51231d7dfa56200cb618b33b38075ed1a
julie rouet <jrouet.work@gmail.com>
2016-04-14 09:16:35 +0200

    add new header with eyes pulse

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-<script>
-    (function(b, o, i, l, e, r) {
-        b.GoogleAnalyticsObject = l;
-        b[l] || (b[l] =
-            function() {
-                (b[l].q = b[l].q || []).push(arguments)
-            });
-        b[l].l = +new Date;
-        e = o.createElement(i);
-        r = o.getElementsByTagName(i)[0];
-        e.src = 'https://www.google-analytics.com/analytics.js';
-        r.parentNode.insertBefore(e, r)
-    }(window, document, 'script', 'ga'));
-    ga('create', 'UA-XXXXX-X', 'auto');
-    ga('send', 'pageview');
-</script>
+
+//transition effet
+$(window).scroll(function() {
+if ($(this).scrollTop() > 150){
+   $('#news').addClass("news");
+ }
+ else{
+     $('#news').removeClass("news");
+ }
+});
+
+
+//initiate the plugin and pass the id of the div containing gallery images
+ $("#zoom_03").elevateZoom({constrainType:"height", constrainSize:274, zoomType: "lens", containLensZoom: true, gallery:'gallery_01', cursor: 'pointer', galleryActiveClass: "active"}); //pass the images to Fancybox
+
+$("#zoom_03").bind("click", function(e) { var ez = $('#zoom_03').data('elevateZoom');	$.fancybox(ez.getGalleryList()); return false; });
